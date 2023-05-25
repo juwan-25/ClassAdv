ad28e847e6585a5360ab32fffea42985a9de6fc1
Juwan Lee <ljw05w5@gmail.com>
2023-05-25 09:05:42 +0900

    소멸자 이해하기

--- ClassAdv/Program.cs
+++ ClassAdv/Program.cs
@@ -10,6 +10,9 @@ namespace ClassAdv
     {
         static void Main(string[] args)
         {
+            Product product1 = new Product("과자", 1000);
+            Product product2 = new Product("아이스크림", 800);
+            Product product3 = new Product("케이크", 10000);
         }
     }
 }
